6b77bfc4730b54bb1e127f4938293da74244e123
Joe Holdcroft <joe@message.co.uk>
2013-07-10 16:33:21 +0100

    Starting on Entity stuff

---
+++ src/Message/Mothership/Commerce/Order/Entity/Collection.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Message\Mothership\Commerce\Order\Entity;
+
+use Message\Mothership\Commerce\Order\Order;
+
+/**
+ * Collection of entities relating to a specific order.
+ *
+ * Entities are lazy loaded on demand.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
+{
+	protected $_order;
+	protected $_loader;
+
+	protected $_items = null;
+
+	public function __construct(Order $order, LoaderInterface $loader)
+	{
+		$this->_order  = $order;
+		$this->_loader = $loader;
+	}
+
+	public function get($id)
+	{
+		$this->_load();
+		// throw exception if it doesn't exist
+		//
+		return $this->_items[$id];
+	}
+
+	public function exists($id)
+	{
+		$this->_load();
+
+		return array_key_exists($id, $this->_items);
+	}
+
+	public function all()
+	{
+		$this->_load();
+
+		return $this->_items;
+	}
+
+	public function count()
+	{
+		$this->_load();
+
+		return count($this->_items);
+	}
+
+	public function offsetSet($id, $value)
+	{
+		throw new \BadMethodCallException('`Entity\Collection` does not allow setting entities using array access');
+	}
+
+	public function offsetGet($id)
+	{
+		$this->_load();
+
+		if (!$this->exists($id)) {
+			throw new \InvalidArgumentException(sprintf('Identifier `%s` does not exist on entity collection', $id));
+		}
+
+		return $this->_items[$id];
+	}
+
+	public function offsetExists($id)
+	{
+		$this->_load();
+
+		return $this->exists($id);
+	}
+
+	public function offsetUnset($id)
+	{
+		$this->_load();
+
+		unset($this->_items[$id]);
+	}
+
+	public function getIterator()
+	{
+		$this->_load();
+
+		return \ArrayIterator($this->_items);
+	}
+
+	protected function _load()
+	{
+		if (null === $this->_items) {
+			$this->_items = $this->_loader->getByOrder($order) ?: array();
+
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ src/Message/Mothership/Commerce/Order/Entity/Item/Loader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Message\Mothership\Commerce\Order\Entity\Item;
+
+use Message\Mothership\Commerce\Order\Entity;
+
+/**
+ * Order item loader.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class Loader implements Entity\LoaderInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getByOrder(Order $order)
+	{
+
+	}
+}
