628281ad4c3e28b9b24cede06f93ea1bea0809a2
Danny Hannah <dannyhannah87@gmail.com>
2013-07-12 16:36:32 +0100

    Fixing address line numbers and fixing null issue

--- src/Message/Mothership/Commerce/User/Address/Edit.php
+++ src/Message/Mothership/Commerce/User/Address/Edit.php
@@ -21,16 +21,15 @@ class Edit
 	{
 		$date = new DateTimeImmutable;
 		$address->authorship->update($date, $this->_user->id);
-
 		$result = $this->_query->run(
 			'UPDATE
 				user_address
 			SET
 				name       = :name?s,
-				line_1     = :line_1?s,
-				line_2     = :line_2?s,
-				line_3     = :line_3?s,
-				line_4     = :line_4?s,
+				line_1     = :line_1?sn,
+				line_2     = :line_2?sn,
+				line_3     = :line_3?sn,
+				line_4     = :line_4?sn,
 				town       = :town?s,
 				state_id   = :state_id?s,
 				country_id = :country_id?s,
@@ -43,10 +42,10 @@ class Edit
 			', array(
 				'addressID'	 => $address->id,
 				'name'       => $address->name,
-				'line_1'     => $address->lines[0],
-				'line_2'     => $address->lines[1],
-				'line_3'     => $address->lines[2],
-				'line_4'     => $address->lines[3],
+				'line_1'     => $address->lines[1],
+				'line_2'     => $address->lines[2],
+				'line_3'     => $address->lines[3],
+				'line_4'     => $address->lines[4],
 				'town'       => $address->town,
 				'state_id'   => $address->stateID,
 				'country_id' => $address->countryID,
