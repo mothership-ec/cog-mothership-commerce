15f98b452d9cf9af662ca64ce89f20f6dee7a86a
Danny Hannah <dannyhannah87@gmail.com>
2013-07-12 16:17:28 +0100

    Adding in Edit decorator

--- src/Message/Mothership/Commerce/User/Address/Edit.php
+++ src/Message/Mothership/Commerce/User/Address/Edit.php
@@ -5,14 +5,54 @@ namespace Message\Mothership\Commerce\User\Address;
 class Edit
 {
 	protected $_query;
+	protected $_user;
 
-	public function __construct(Query $query)
+	public function __construct(Query $query, User $user)
 	{
 		$this->_query = $query;
+		$this->_user  = $user;
 	}
 
 	public function save(Address $address)
 	{
-		# code...
+		$date = new DateTimeImmutable;
+		$address->authorship->update($date, $this->_user->id);
+
+		$result = $this->_query->run(
+			'UPDATE
+				user_address
+			SET
+				name       = :name?s,
+				line_1     = :line_1?s,
+				line_2     = :line_2?s,
+				line_3     = :line_3?s,
+				line_4     = :line_4?s,
+				town       = :town?s,
+				state_id   = :state_id?s,
+				country_id = :country_id?s,
+				postcode   = :postcode?s,
+				telephone  = :telephone?s,
+				updated_at = :updated_at?d,
+				update_by  = :update_by?i
+			WHERE
+				address_id = :addressID?i
+			', array(
+				'addressID'	 => $address->id,
+				'name'       => $address->name,
+				'line_1'     => $address->line[1],
+				'line_2'     => $address->line[2],
+				'line_3'     => $address->line[3],
+				'line_4'     => $address->line[4],
+				'town'       => $address->town,
+				'state_id'   => $address->stateID,
+				'country_id' => $address->countryID,
+				'postcode'   => $address->postcode,
+				'telephone'  => $address->telephone,
+				'updated_at' => $address->authorship->updatedAt(),
+				'update_by'  => $address->authorship->updateBy(),
+			)
+		);
+
+		return $address;
 	}
 }
