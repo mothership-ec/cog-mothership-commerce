36ad21d5a1fd9ac808b56a8b0b4316c9c0f7863c
Joe Holdcroft <joe@message.co.uk>
2013-07-15 11:33:26 +0100

    Moving Exception to its own namespace

---
+++ src/Message/Mothership/Commerce/Order/Exception/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Message\Mothership\Commerce\Order\Exception;
+
+/**
+ * Base order exception.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class Exception extends \Exception
+{
+
+}
