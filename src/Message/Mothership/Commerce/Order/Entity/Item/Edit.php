dd6bc80b8ce615db18de25ab71a7874a6d0ec506
Joe Holdcroft <joe@message.co.uk>
2013-07-25 15:19:52 +0100

    Adding Item edit decorator

---
+++ src/Message/Mothership/Commerce/Order/Entity/Item/Edit.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Message\Mothership\Commerce\Order\Entity\Item;
+
+use Message\Mothership\Commerce\Order\Entity\TransactionalDecoratorInterface;
+use Message\Mothership\Commerce\Order\Status\Collection as StatusCollection;
+
+use Message\User\UserInterface;
+
+use Message\Cog\DB;
+use Message\Cog\ValueObject\DateTimeImmutable;
+
+class Edit
+{
+	protected $_query;
+	protected $_statuses;
+	protected $_currentUser;
+
+	public function __construct(DB\Query $query, StatusCollection $statuses, UserInterface $currentUser)
+	{
+		$this->_query = $query;
+		$this->_statuses = $statuses;
+		$this->_currentUser = $currentUser;
+	}
+
+	public function updateStatus(Item $item, $statusCode)
+	{
+		if (!$this->_statuses->exists($statusCode)) {
+			throw new \InvalidArgumentException(sprintf('Order item status `%s` does not exist', $statusCode));
+		}
+
+		$status = $this->_statuses->get($statusCode);
+
+		// Skip if the item is already at this status
+		if ($status->code === $item->status->code) {
+			return false;
+		}
+
+		$status = new Status\Status($status->code, $status->name);
+
+		$status->authorship->create(
+			new DateTimeImmutable,
+			$this->_currentUser->id
+		);
+
+		$this->_query->run('
+			INSERT INTO
+				order_item_status
+			SET
+				order_id    = :orderID?i,
+				item_id     = :itemID?i,
+				status_code = :status?i,
+				created_at  = :createdAt?i,
+				created_by  = :createdBy?in
+		', array(
+			'orderID'   => $item->order->id,
+			'itemID'    => $item->id,
+			'status'    => $status->code,
+			'createdAt' => $status->authorship->createdAt(),
+			'createdBy' => $status->authorship->createdBy(),
+		));
+
+		$item->status = $status;
+
+		return $item;
+	}
+}
