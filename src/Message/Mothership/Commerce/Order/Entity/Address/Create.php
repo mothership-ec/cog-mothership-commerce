fc4fe870c1c7bbb8dfb1079963dfa91ae0fcdff8
Joe Holdcroft <joe@message.co.uk>
2013-07-31 11:14:48 +0100

    Address creating functionality

--- src/Message/Mothership/Commerce/Order/Entity/Address/Create.php
+++ src/Message/Mothership/Commerce/Order/Entity/Address/Create.php
@@ -25,8 +25,44 @@ class Create implements DB\TransactionalInterface
 		$this->_query = $trans;
 	}
 
-	public function create(Address $entity)
+	public function create(Address $address)
 	{
+		$this->_query->add('
+			INSERT INTO
+				order_address
+			SET
+				order_id   = :orderID?i,
+				type       = :type?s,
+				name       = :name?sn,
+				line_1     = :line1?sn,
+				line_2     = :line2?sn,
+				line_3     = :line3?sn,
+				line_4     = :line4?sn,
+				postcode   = :postcode?sn,
+				country    = :country?sn,
+				country_id = :countryID?sn,
+				telephone  = :telephone?sn,
+				town       = :town?sn,
+				state_id   = :stateID?sn,
+				state      = :state?sn
+		', array(
+			'orderID'   => $address->order->id,
+			'type'      => $address->type,
+			'name'      => $address->name,
+			'line1'     => $address->lines[1],
+			'line2'     => $address->lines[2],
+			'line3'     => $address->lines[3],
+			'line4'     => $address->lines[4],
+			'postcode'  => $address->postcode,
+			'country'   => $address->country,
+			'countryID' => $address->countryID,
+			'telephone' => $address->telephone,
+			'town'      => $address->town,
+			'stateID'   => $address->stateID,
+			'state'     => $address->state,
+		));
 
+		// use address loader to re-load this item and return it ONLY IF NOT IN ORDER CREATION TRANSACTION
+		return $address;
 	}
 }
