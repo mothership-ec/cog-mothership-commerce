d5dde2c08ef6c9145d09a9cce6e79f03b83f754d
Joe Holdcroft <joe@message.co.uk>
2013-08-09 13:17:46 +0100

    Adding products back to the main menu

--- src/Message/Mothership/Commerce/EventListener.php
+++ src/Message/Mothership/Commerce/EventListener.php
@@ -40,7 +40,7 @@ class EventListener extends BaseListener implements SubscriberInterface
 	 */
 	public function registerMainMenuItems(BuildMenuEvent $event)
 	{
-		//$event->addItem('ms.commerce.product.dashboard', 'Products', array('ms.products'));
+		$event->addItem('ms.commerce.product.dashboard', 'Products', array('ms.products'));
 		$event->addItem('ms.commerce.order.view.dashboard', 'Orders', array('ms.order'));
 	}
 
