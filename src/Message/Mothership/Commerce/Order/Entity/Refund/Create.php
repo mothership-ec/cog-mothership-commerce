011fe70ad70ee86e953530ae2d191673d6088b38
Laurence Roberts <lsjroberts@gmail.com>
2013-08-14 13:18:46 +0100

    Added sql insert to order refund create decorator

--- src/Message/Mothership/Commerce/Order/Entity/Refund/Create.php
+++ src/Message/Mothership/Commerce/Order/Entity/Refund/Create.php
@@ -38,11 +38,40 @@ class Create implements DB\TransactionalInterface
 
 		$this->_validate($refund);
 
-		
+		$this->_query->run('
+			INSERT INTO
+				order_refund
+			SET
+				order_id = ?i,
+				payment_id = ?i,
+				return_id = ?i,
+				created_at = ?i,
+				created_by = ?i,
+				method = ?s,
+				amount = ?f,
+				reason = ?s,
+				reference = ?s
+		', array(
+			$refund->order->id,
+			($refund->payment) ? $refund->payment->id : 0,
+			($refund->return) ? $refund->return->id : 0,
+			$refund->authorship->createdAt(),
+			$refund->authorship->createdBy(),
+			$refund->method,
+			$refund->amount,
+			$refund->reason,
+			$refund->reference
+		));
+
+		return $refund;
 	}
 
 	protected function _validate(Refund $refund)
 	{
+		if (! $refund->order) {
+			throw new InvalidArgumentException('Could not create refund: no order specified');
+		}
+
 		if ($refund->amount <= 0) {
 			throw new InvalidArgumentException('Could not create refund: amount must be greater than 0');
 		}
