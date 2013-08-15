6b23d7798cdb431bf6b4cabbf58355393f704e32
Iris Schaffer <irisSchaffer@gmx.net>
2013-08-15 11:18:13 +0100

    Add create-class for stock movement adjustments

---
+++ src/Message/Mothership/Commerce/Product/Stock/Movement/Adjustment/Create.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Message\Mothership\Commerce\Product\Stock\Movement\Adjustment;
+
+use Message\Mothership\Commerce\Product\Stock\Movement\Adjustment\Adjustment;
+
+use Message\User\UserInterface;
+
+use Message\Cog\DB;
+
+/**
+ * Stock movement adjustment creator.
+ */
+class Create implements DB\TransactionalInterface
+{
+	protected $_query;
+
+	public function __construct(DB\Transaction $query)
+	{
+		$this->_query       = $query;
+	}
+
+	public function setTransaction(DB\Transaction $trans)
+	{
+		$this->_query = $trans;
+	}
+
+	public function create(Adjustment $adjustment)
+	{
+		$this->_query->add('
+			INSERT INTO
+				stock_movement_adjustment
+			SET
+				stock_movement_id 	= :movementID?i,
+				unit_id  			= :unitID?i,
+				location 			= :location?s,
+				delta    			= :delta?i
+		', array(
+			'movementID' => $adjustment->movement->id,
+			'unitID' 	 => $adjustment->unit->id,
+			'location'   => $adjustment->location->name,
+			'delta'   	 => $adjustment->delta,
+		));
+
+		return $adjustment;
+	}
+}
