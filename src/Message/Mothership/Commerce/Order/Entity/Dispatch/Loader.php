e8b1254447324c18b32fdaf892bf379dafb6a4aa
Joe Holdcroft <joe@message.co.uk>
2013-09-03 12:00:02 +0100

    Adding method to get postage & unshipped dispatches

--- src/Message/Mothership/Commerce/Order/Entity/Dispatch/Loader.php
+++ src/Message/Mothership/Commerce/Order/Entity/Dispatch/Loader.php
@@ -72,7 +72,39 @@ class Loader extends Order\Entity\BaseLoader
 				WHERE
 					code IS NULL
 				AND shipped_at IS NULL
+			');
+		}
+
+		return $this->_load($result->flatten(), true);
+	}
+
+	public function getPostagedUnshipped($method = null)
+	{
+		if ($method instanceof MethodInterface) {
+			$method = $method->getName();
+		}
+
+		if ($method) {
+			$result = $this->_query->run('
+				SELECT
+					dispatch_id
+				FROM
+					order_dispatch
+				WHERE
+					code IS NOT NULL
+				AND shipped_at IS NULL
 				AND method = ?s
+			', array($method));
+		}
+		else {
+			$result = $this->_query->run('
+				SELECT
+					dispatch_id
+				FROM
+					order_dispatch
+				WHERE
+					code IS NOT NULL
+				AND shipped_at IS NULL
 			');
 		}
 
