42516826e24e51d58a9464af9ea02e76a92b49cd
Iris Schaffer <irisSchaffer@gmx.net>
2013-09-17 17:06:44 +0100

    fixed product tabs active-state; closes messagedigital/cog-mothership-cp/#87

--- src/Message/Mothership/Commerce/Controller/Product/Tabs.php
+++ src/Message/Mothership/Commerce/Controller/Product/Tabs.php
@@ -6,10 +6,23 @@ use Message\Cog\Controller\Controller;
 
 class Tabs extends Controller
 {
-	public function index($productID = null)
+	public function index($productID)
 	{
+		$tabs = array(
+			'Attributes' => 'ms.commerce.product.edit.attributes',
+			'Details'	 => 'ms.commerce.product.edit.details',
+			'Pricing' 	 => 'ms.commerce.product.edit.pricing',
+			'Units'  	 => 'ms.commerce.product.edit.units',
+			'Stock'  	 => 'ms.commerce.product.edit.stock',
+			'Images'  	 => 'ms.commerce.product.edit.images',
+		);
+
+		$current = $this->get('http.request.master')->get('_route');
+
 		return $this->render('Message:Mothership:Commerce::product:tabs', array(
-			'productID' => $productID,
+			'tabs'    	  => $tabs,
+			'current' 	  => $current,
+			'productID'   => $productID,
 		));
 	}
 }
