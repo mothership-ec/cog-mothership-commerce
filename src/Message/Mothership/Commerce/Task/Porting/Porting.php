83e279af32003a6bf00758730f3c1cfb35db27fd
Danny Hannah <dannyhannah87@gmail.com>
2013-09-20 10:44:37 +0100

    Adding in porting configs

--- src/Message/Mothership/Commerce/Task/Porting/Porting.php
+++ src/Message/Mothership/Commerce/Task/Porting/Porting.php
@@ -14,10 +14,10 @@ abstract class Porting extends BaseTask
 	public function getFromConnection()
 	{
         return new \Message\Cog\DB\Adapter\MySQLi\Connection(array(
-				'host'		=> '192.168.201.99',
-				'user'		=> 'danny',
-				'password' 	=> 'chelsea',
-				'db'		=> 'uniform_wares',
+				'host'		=> $this->get('cfg')->porting->from->hostname,
+				'user'		=> $this->get('cfg')->porting->from->user,
+				'password' 	=> $this->get('cfg')->porting->from->password,
+				'db'		=> $this->get('cfg')->porting->from->name,
 				'charset'	=> 'utf-8',
 		));
 	}
@@ -30,10 +30,10 @@ abstract class Porting extends BaseTask
 	public function getToConnection()
 	{
 		return new \Message\Cog\DB\Adapter\MySQLi\Connection(array(
-				'host'		=> '192.168.201.99',
-				'user'		=> 'danny',
-				'password' 	=> 'chelsea',
-				'db'		=> 'mothership_cms',
+				'host'		=> $this->get('cfg')->db->hostname,
+				'user'		=> $this->get('cfg')->db->user,
+				'password' 	=> $this->get('cfg')->db->password,
+				'db'		=> $this->get('cfg')->db->name,
 				'charset'	=> 'utf-8',
 		));
 	}
