5b6cd4a33be87b4063af1be2ee46b27c317f579f
Iris Schaffer <irisSchaffer@gmx.net>
2013-10-20 15:07:29 +0200

    Order: Check whether order exists added - fixes #159

--- src/Message/Mothership/Commerce/Controller/Order/OrderDetail.php
+++ src/Message/Mothership/Commerce/Controller/Order/OrderDetail.php
@@ -19,7 +19,7 @@ class OrderDetail extends Controller
 
 	public function orderOverview($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_addresses = $this->get('order.address.loader')->getByOrder($this->_order);
 		$this->_items = $this->get('order.item.loader')->getByOrder($this->_order);
 		$this->_metadata = $this->_order->metadata;
@@ -34,7 +34,7 @@ class OrderDetail extends Controller
 
 	public function addressListing($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_addresses = $this->get('order.address.loader')->getByOrder($this->_order);
 		return $this->render('::order:detail:address:listing', array(
 			'order' => $this->_order,
@@ -44,7 +44,7 @@ class OrderDetail extends Controller
 
 	public function itemListing($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_items = $this->get('order.item.loader')->getByOrder($this->_order);
 
 		$statuses = array();
@@ -61,7 +61,7 @@ class OrderDetail extends Controller
 
 	public function paymentListing($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_payments = $this->get('order.payment.loader')->getByOrder($this->_order);
 		return $this->render('::order:detail:payment:listing', array(
 			'order' => $this->_order,
@@ -71,7 +71,7 @@ class OrderDetail extends Controller
 
 	public function dispatchListing($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_dispatches = $this->get('order.dispatch.loader')->getByOrder($this->_order);
 		return $this->render('::order:detail:dispatch:listing', array(
 			'order' => $this->_order,
@@ -81,7 +81,7 @@ class OrderDetail extends Controller
 
 	public function noteListing($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$this->_notes = $this->get('order.note.loader')->getByOrder($this->_order);
 
 		$createNoteForm = $this->_createNoteForm($orderID);
@@ -95,19 +95,24 @@ class OrderDetail extends Controller
 
 	public function processNote($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = $this->_getAndCheckOrder($orderID);
 		$form = $this->_createNoteForm($orderID);
 
 		if ($form->isValid() and $data = $form->getFilteredData()) {
 			$note = new Note;
 			$note->order = $this->_order;
 			$note->note = $data['note'];
+
+			// TODO 'order_view' should be defined somewhere!
 			$note->raisedFrom = 'order_view';
 			$note->customerNotified = $data['customer_notified'];
 
 			$this->get('order.note.create')->create($note);
 
-			$this->addFlash('success', 'The note has been added the order');
+			$this->addFlash(
+				'success',
+				$this->trans('ms.commerce.order.feedback.create-note.success')
+			);
 		}
 
 		return $this->redirectToRoute('ms.commerce.order.detail.view.notes', array(
@@ -117,7 +122,7 @@ class OrderDetail extends Controller
 
 	public function documentListing($orderID)
 	{
-		$this->_order     = $this->get('order.loader')->getById($orderID);
+		$this->_order     = $this->_getAndCheckOrder($orderID);
 		$this->_documents = $this->get('order.document.loader')->getByOrder($this->_order);
 		return $this->render('::order:detail:document:listing', array(
 			'order' => $this->_order,
@@ -127,7 +132,7 @@ class OrderDetail extends Controller
 
 	public function sidebar($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = ($this->_order ?: $this->_getAndCheckOrder($orderID));
 
 		return $this->render('Message:Mothership:Commerce::order:detail:sidebar', array(
 			'order' => $this->_order,
@@ -136,7 +141,7 @@ class OrderDetail extends Controller
 
 	public function tabs($orderID)
 	{
-		$this->_order = $this->get('order.loader')->getById($orderID);
+		$this->_order = ($this->_order ?: $this->_getAndCheckOrder($orderID));
 
 		$event = new BuildOrderTabsEvent($this->_order);
 
@@ -153,9 +158,9 @@ class OrderDetail extends Controller
 		));
 	}
 
-	public function _createNoteForm($orderID)
+	protected function _createNoteForm($orderID)
 	{
-		$this->_order = ($this->_order) ?: $this->get('order.loader')->getById($orderID);
+		$this->_order = ($this->_order ?: $this->_getAndCheckOrder($orderID));
 
 		$form = $this->get('form');
 		$form->setAction($this->generateUrl('ms.commerce.order.detail.process.notes', array(
@@ -168,5 +173,22 @@ class OrderDetail extends Controller
 			->val()->optional();
 
 		return $form;
+	}
+
+	protected function _getAndCheckOrder($orderID) {
+		$order = $this->get('order.loader')->getById($orderID);
+
+		if (!$order) {
+			throw $this->createNotFoundException(
+				$this->trans(
+					'ms.commerce.order.feedback.general.failure.non-existing-order',
+					array('%orderID%' => $orderID)
+				),
+				null,
+				404
+			);
+		}
+
+		return $order;
 	}
 }
