1caede5cb4fe6bb529f1409d74afacafe0d98dba
Joe Holdcroft <joe@message.co.uk>
2013-10-28 12:09:53 +0000

    Adding base Events class for Commerce

---
+++ src/Message/Mothership/Commerce/Events.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Message\Mothership\Commerce;
+
+final class Events
+{
+	const PRODUCT_SELECTOR_BUILD   = 'commerce.product_selector.form.build';
+	const PRODUCT_SELECTOR_PROCESS = 'commerce.product_selector.form.process';
+}
