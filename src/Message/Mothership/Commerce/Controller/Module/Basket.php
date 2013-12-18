0bdf75d99214cc327761be882ea53b48307fc307
Joe Holdcroft <joe@message.co.uk>
2013-12-18 16:49:23 +0000

    Adding basket controller
    Might seem mad. But see messagedigital/cog-mothership-ecommerce#226

---
+++ src/Message/Mothership/Commerce/Controller/Module/Basket.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Message\Mothership\Commerce\Controller\Module;
+
+use Message\Cog\Controller\Controller;
+
+class Basket extends Controller
+{
+	public function display()
+	{
+		$order          = $this->get('basket')->getOrder();
+		$totalListPrice = 0;
+
+		foreach ($order->items as $item) {
+			$totalListPrice += $item->listPrice;
+		}
+
+		return $this->render('Message:Mothership:Commerce::basket', array(
+			'order'          => $order,
+			'totalListPrice' => $totalListPrice,
+		));
+	}
+}
