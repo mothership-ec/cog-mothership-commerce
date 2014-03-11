affcaf72a5953b254af9a7ac42caadba9e2a35c2
Iris Schaffer <irisSchaffer@gmx.net>
2014-03-11 13:28:25 +0000

    Adding ProductPageMapper again - no idea where it went...

---
+++ src/ProductPageMapper/PageProductMapperInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Message\Mothership\Commerce\ProductPageMapper;
+
+use Message\Mothership\CMS\Page\Page;
+use Message\Mothership\Commerce\Product\Product;
+use Message\Mothership\Commerce\Product\Unit\Unit;
+
+interface PageProductMapperInterface
+{
+	/**
+	 * Get the units associated with a page.
+	 *
+	 * @param  Page   $page
+	 * @return array[Unit]
+	 */
+	public function getProductUnitsForPage(Page $page);
+
+	/**
+	 * Get the first product associated with a page.
+	 *
+	 * @param  Page   $page
+	 * @return array[Product]
+	 */
+	public function getProductForPage(Page $page);
+
+	/**
+	 * Get the products associated with a page.
+	 *
+	 * @param  Page     $page
+	 * @return array[Product]
+	 */
+	public function getProductsForPage(Page $page);
+}
