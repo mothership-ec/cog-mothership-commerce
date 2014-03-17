c1ab088af0530b74ad9a62285d25a19e44a8b5a1
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-03-17 14:49:37 +0000

    set methods on apparel product type

--- src/Product/Type/ApparelProductType.php
+++ src/Product/Type/ApparelProductType.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thomas
- * Date: 17/03/2014
- * Time: 14:25
- */
+
+namespace Message\Mothership\Commerce\Product\Type;
+
+use Message\Mothership\Commerce\Product\Product;
+use Message\Cog\DB\Query;
+use Message\Cog\Field\Factory;
+
+class ApparelProductType implements ProductTypeInterface
+{
+	public function getName()
+	{
+		return 'apparel';
+	}
+
+	public function getDisplayName()
+	{
+		return 'Apparel product';
+	}
+
+	public function getDescription()
+	{
+		return 'A wearable item e.g. clothing';
+	}
+
+	public function setFields(Factory $factory)
+	{
+		
+	}
+
+	public function getProductDisplayName(Product $product = null)
+	{
+
+	}
+}
