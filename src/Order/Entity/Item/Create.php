393e6d3fdcc936c00f8ddb996dca36fa087d2d2f
Iris Schaffer <irisSchaffer@gmx.net>
2014-04-25 11:04:10 +0100

    Fixing IDs

--- src/Order/Entity/Item/Create.php
+++ src/Order/Entity/Item/Create.php
@@ -39,10 +39,18 @@ class Create implements DB\TransactionalInterface
 		$this->_currentUser     = $currentUser;
 	}
 
+	/**
+	 * Sets transaction and sets $_transOverridden to true.
+	 * 
+	 * @param  DB\Transaction $trans Transaction
+	 * @return Create                $this for chainability
+	 */
 	public function setTransaction(DB\Transaction $trans)
 	{
 		$this->_query          = $trans;
 		$this->_transOverridden = true;
+
+		return $this;
 	}
 
 	public function create(Item $item)
@@ -118,8 +126,10 @@ class Create implements DB\TransactionalInterface
 			'stockLocation'  => $item->stockLocation->name,
 		));
 
-		$this->_query->setIDVariable('ITEM_ID');
-		$item->id = '@ITEM_ID';
+		$sqlVariable = 'ITEM_ID_' . spl_object_hash($item);
+
+		$this->_query->setIDVariable($sqlVariable);
+		$item->id = '@' . $sqlVariable;
 
 		// Set the initial status, if defined
 		if ($item->status) {
@@ -176,7 +186,7 @@ class Create implements DB\TransactionalInterface
 		if (!$this->_transOverridden) {
 			$this->_query->commit();
 
-			return $this->_loader->getByID($this->_query->getIDVariable('ITEM_ID'), $item->order);
+			return $this->_loader->getByID($this->_query->getIDVariable($sqlVariable), $item->order);
 		}
 
 		return $item;
