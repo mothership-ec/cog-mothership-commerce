b62b01039abbcc9fab44ab32bab205e18301d110
Laurence Roberts <lsjroberts@gmail.com>
2014-05-06 11:32:37 +0100

    Fixed total sales dashboard graph

--- src/Controller/Module/Dashboard/TotalSales.php
+++ src/Controller/Module/Dashboard/TotalSales.php
@@ -22,9 +22,32 @@ class TotalSales extends Controller
 		$net      = $dataset->getRange($dataset::WEEK);
 		$totalNet = $dataset->getTotal($dataset::WEEK);
 
+		$rows = [];
+
+		// Pre-fill rows with a total of 0 on each day since the first to
+		// ensure we don't lose days where there are is no value.
+		$first = key($net);
+		$day = 60*60*24;
+		for ($i = $first; $i < $first + $day * 6; $i += $day) {
+			$label = date('l', $i);
+			$rows[$label] = ['label' => $label, 'value' => 0.0];
+		}
+
+		foreach ($net as $timestamp => $value) {
+			$label = date('l', $timestamp);
+			$rows[$label] = [
+				'label' => $label,
+				'value' => (float) $value
+			];
+		}
+
 		return $this->render('Message:Mothership:ControlPanel::module:dashboard:area-graph', [
 			'label'   => 'Total sales (week)',
-			'rows'  => $net,
+			'keys' => [
+				'label' => 'Day',
+				'value' => 'Amount',
+			],
+			'rows'  => $rows,
 			'numbers' => [
 				[
 					'value'   => $totalNet,
