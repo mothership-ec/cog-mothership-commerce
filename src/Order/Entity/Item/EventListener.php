b825f9a3b69985f3f8598b4153330b4f0e674fdd
Joe Holdcroft <joe@message.co.uk>
2014-06-02 17:29:36 +0100

    Set base price for order items

--- src/Order/Entity/Item/EventListener.php
+++ src/Order/Entity/Item/EventListener.php
@@ -25,6 +25,7 @@ class EventListener implements SubscriberInterface
 		return array(
 			OrderEvents::ENTITY_CREATE => array(
 				array('setDefaultActualPrice'),
+				array('setBasePrice'),
 				array('calculateTax'),
 				array('setDefaultStatus'),
 			),
@@ -33,6 +34,7 @@ class EventListener implements SubscriberInterface
 			),
 			OrderEvents::ASSEMBLER_UPDATE => array(
 				array('setDefaultActualPrice'),
+				array('setBasePrice'),
 				array('calculateAllItemsTax'),
 			),
 		);
@@ -67,6 +69,38 @@ class EventListener implements SubscriberInterface
 			if (!$item->actualPrice) {
 				$item->actualPrice = $item->listPrice;
 			}
+		}
+	}
+
+	/**
+	 * Set the `basePrice` value for the item(s).
+	 *
+	 * Base price is the same as the actual price unless the strategy is
+	 * `inclusive` and the order is not taxable (i.e. a tax discount must be
+	 * applied).
+	 *
+	 * @param Event\Event $event
+	 */
+	public function setBasePrice(Event\Event $event)
+	{
+		if ($event instanceof Event\EntityEvent
+		 && $event->getEntity() instanceof Item) {
+			$items = [$event->getEntity()];
+		} else {
+			$items = $event->getOrder()->items->all();
+		}
+
+		foreach ($items as $item) {
+			$item->basePrice = $item->actualPrice;
+
+			// Skip if tax strategy is exclusive or the order is taxable
+			if ('exclusive' === $item->taxStrategy
+			 || true === $item->order->taxable) {
+				continue;
+			}
+
+			// Remove the tax discount
+			$item->basePrice -= $this->_calculateInclusiveTax($item->actualPrice, $item->productTaxRate);
 		}
 	}
 
