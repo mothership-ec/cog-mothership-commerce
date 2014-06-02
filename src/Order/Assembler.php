da7421c682deebd1bb3f65531d7371e5967c402c
Joe Holdcroft <joe@message.co.uk>
2014-06-02 11:21:24 +0100

    Fix bug where basket only took 1 item

--- src/Order/Assembler.php
+++ src/Order/Assembler.php
@@ -182,7 +182,14 @@ class Assembler
 			$this->_prepareEntity($name, $entity);
 		}
 
-		$this->_order->{$name}->remove($entity->id);
+		// If we have IDs for these entities, remove using that
+		if (array_key_exists($name, $this->_entityTemporaryIdFields)) {
+			$this->_order->{$name}->remove($entity->id);
+		}
+		// Otherwise, remove using the entity instance
+		else {
+			$this->_order->{$name}->remove($entity);
+		}
 
 		return $this->dispatchEvent();
 	}
