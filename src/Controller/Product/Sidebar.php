054a7a81a07a5c5e7594e6dec138f3034d0843a4
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-06-04 15:55:29 +0100

    uncomment the thing ic ommented once upon a time

--- src/Controller/Product/Sidebar.php
+++ src/Controller/Product/Sidebar.php
@@ -8,14 +8,13 @@ class Sidebar extends Controller
 {
 	public function index($productID = null)
 	{
-		// @todo uncomment this stuff. Do NOT let it pass QA like this yo
-//		$products = $this->get('product.loader')->getAll();
-//		$listing  = array();
-//
-//		foreach ($products as $product) {
-//			$listing[$product->category][] = $product;
-//		}
-		$listing = [];
+		$products = $this->get('product.loader')->getAll();
+		$listing  = array();
+
+		foreach ($products as $product) {
+			$listing[$product->category][] = $product;
+		}
+
 		return $this->render('Message:Mothership:Commerce::product:sidebar', array(
 			'listing'   => $listing,
 			'productID' => $productID,
