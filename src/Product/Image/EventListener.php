4fbacf93407153cdab5219021a4d0af1d38ee6e9
Samuel Trangmar-Keates <sam@Samuels-MacBook-Air.local>
2014-07-18 17:31:05 +0100

    EventListner for Image deletion

---
+++ src/Product/Image/EventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Message\Mothership\Commerce\Product\Image;
+
+use Message\Cog\Event\SubscriberInterface;
+use Message\Mothership\FileManager\File;
+use Message\Mothership\FileManager\File\Event as FileEvent;
+use Message\Cog\Event\EventListener as BaseListener;
+
+/**
+ * Image event listener.
+ *
+ * @author Sam Trangmar-Keates <sam@message.co.uk>
+ */
+class EventListener extends BaseListener implements SubscriberInterface {
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	static public function getSubscribedEvents()
+	{
+		return array(
+			FileEvent::DELETE => [
+				['deleteImage'],
+			],
+		);
+	}
+
+	/**
+	 * Delete the given image
+	 * @param  Image  $image
+	 */
+	public function deleteImage (FileEvent $e)
+	{
+		$delete = $this->_services['product.image.delete'];
+		$delete->delete($e->getFile()->checksum);
+	}
+}
