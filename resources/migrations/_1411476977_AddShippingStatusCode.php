68a19f242a15e2c47c930343eb966a23f568f1cf
Samuel Trangmar-Keates <sam@message.co.uk>
2014-09-24 09:47:49 +0100

    Shipping status migration

---
+++ resources/migrations/_1411476977_AddShippingStatusCode.php
@@ -0,0 +1,16 @@
+<?php
+
+use Message\Cog\Migration\Adapter\MySQL\Migration;
+
+class _1411476977_AddShippingStatusCode extends Migration
+{
+	public function up()
+	{
+		$this->run("ALTER TABLE order_shipping ADD status INT(11) SIGNED  NULL  DEFAULT NULL; ");
+	}
+
+	public function down()
+	{
+		$this->run("ALTER TABLE order_shipping DROP COLUMN status; ");
+	}
+}
