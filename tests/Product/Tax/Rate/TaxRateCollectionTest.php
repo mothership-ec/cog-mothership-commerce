6fee89bdf2efff31dd86d130a25c1bbf770bc87b
Samuel Trangmar-Keates <sam@message.co.uk>
2014-11-05 12:57:00 +0000

    axRateCollection test

---
+++ tests/Product/Tax/Rate/TaxRateCollectionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Message\Mothership\Commerce\Test\Product\Tax\Rate;
+
+use Message\Mothership\Commerce\Product\Tax\Rate\TaxRate;
+use Message\Mothership\Commerce\Product\Tax\Rate\TaxRateCollection;
+
+class TaxRateCollectionTest extends \PHPUnit_Framework_TestCase
+{
+	public function testGetProductTaxes()
+	{
+		$taxes = [
+			new TaxRate(20, 'PST', 't1'),
+			new TaxRate(30, 'PST', 't2'),
+			new TaxRate(40, 'PST', 't3'),
+		];
+
+		$collection = new TaxRateCollection($taxes);
+
+		var_dump($collection->getTotalTaxRate());
+	}
+}
