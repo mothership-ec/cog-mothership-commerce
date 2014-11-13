cf1fcb76e7bc2933713fd55c1771d0350c57ecfa
Samuel Trangmar-Keates <sam@message.co.uk>
2014-11-13 16:20:57 +0000

    Migration for order item taxes

---
+++ resources/migrations/_1415804834_OredrItemTaxRates.php
@@ -0,0 +1,23 @@
+<?php
+
+use Message\Cog\Migration\Adapter\MySQL\Migration;
+
+class _1411476977_AddShippingStatusCode extends Migration
+{
+	public function up()
+	{
+		$this->run("
+			CREATE TABLE IF NOT EXISTS `order_item_tax` (
+			  `item_id` int(11) unsigned NOT NULL,
+			  `tax_type` varchar(30) NOT NULL,
+			  `tax_rate` decimal(10,2) unsigned NOT NULL,
+			  PRIMARY KEY (`item_id`,`tax_type`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		");
+	}
+
+	public function down()
+	{
+		$this->run("DROP TABLE IF EXISTS `order_item_tax`; ");
+	}
+}
