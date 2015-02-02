7cb222df98ecbcc20ba1bd6c14405ce2155889eb
Samuel Trangmar-Keates <sam@message.co.uk>
2015-02-02 12:28:07 +0000

    id is state filter

--- resources/assets/js/state-filter.js
+++ resources/assets/js/state-filter.js
@@ -1,7 +1,7 @@
 $(function() {
-	$('select[data-state-filter-country-selector]').filteredSelect({
+	$('select[data-state-filter-country-id]').filteredSelect({
 		monitor: function() {
-			return $($(this).attr('data-state-filter-country-selector'));
+			return $($(this).attr('data-state-filter-country-id'));
 		},
 		onChange: function() {
 			if ($(this).children('option[value!=""]').length == 0) {
