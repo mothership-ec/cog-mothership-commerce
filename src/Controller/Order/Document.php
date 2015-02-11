ce6d3554be996a69837de3d1ef4a9f9dcc101bde
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-02-11 16:28:57 +0000

    delete call to ecommerce jzebra controller

--- src/Controller/Order/Document.php
+++ src/Controller/Order/Document.php
@@ -11,14 +11,6 @@ class Document extends Controller
 	{
 		$document = $this->get('order.document.loader')->getByID($documentID);
 
-		if ($document->type == 'dispatch-label') {
-
-			// @temp Should be moved to file manager
-			return $this->render('Message:Mothership:Ecommerce::print-jzebra', array(
-				'contents' => explode("\n", file_get_contents($document->file->getRealPath())),
-			));
-		}
-
 		return $this->redirectToRoute('ms.cp.file_manager.print', array(
 			'path' => $document->file->getRealPath()
 		));
