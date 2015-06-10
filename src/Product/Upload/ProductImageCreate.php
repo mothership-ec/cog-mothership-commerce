c9e2d068b34b817d84d620999ae01a95821a8817
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-06-10 16:38:16 +0100

    docblock on ProductImageCreate

--- src/Product/Upload/ProductImageCreate.php
+++ src/Product/Upload/ProductImageCreate.php
@@ -4,6 +4,14 @@ namespace Message\Mothership\Commerce\Product\Upload;
 
 use Message\Mothership\Commerce\Product;
 
+/**
+ * Class ProductImageCreate
+ * @package Message\Mothership\Commerce\Product\Upload
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Class for using data from CSV to load and an image to the newly created product, and save it
+ */
 class ProductImageCreate
 {
 	const HEADING_KEY = 'defaultImage';
@@ -23,6 +31,11 @@ class ProductImageCreate
 	 */
 	private $_headingKeys;
 
+	/**
+	 * @param Product\Image\Assignor $imageAssignor
+	 * @param Product\Image\Create $imageCreate
+	 * @param HeadingKeys $headingKeys
+	 */
 	public function __construct(Product\Image\Assignor $imageAssignor, Product\Image\Create $imageCreate, HeadingKeys $headingKeys)
 	{
 		$this->_imageAssignor = $imageAssignor;
@@ -30,6 +43,14 @@ class ProductImageCreate
 		$this->_headingKeys   = $headingKeys;
 	}
 
+	/**
+	 * Assign the image to the product using the filename given in the CSV, and save it against the product
+	 *
+	 * @param Product\Product $product
+	 * @param array $row
+	 * @param array $options
+	 * @param string $type
+	 */
 	public function save(Product\Product $product, array $row, $options = [], $type = 'default')
 	{
 		$key = $this->_headingKeys->getKey(self::HEADING_KEY);
