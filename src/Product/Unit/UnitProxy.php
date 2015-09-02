a874845338e7bc385b40c7ad36a6b733006eda0f
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-09-02 11:56:53 +0100

    docblocks for UnitProxy

--- src/Product/Unit/UnitProxy.php
+++ src/Product/Unit/UnitProxy.php
@@ -4,12 +4,25 @@ namespace Message\Mothership\Commerce\Product\Unit;
 
 use Message\Cog\Localisation\Locale;
 use Message\Cog\DB\Entity\EntityLoaderCollection;
-use Message\Mothership\Commerce\Product\Product;
 
+/**
+ * Class UnitProxy
+ * @package Message\Mothership\Commerce\Product\Unit
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Proxy class for units, so that products can be lazy loaded.
+ */
 class UnitProxy extends Unit
 {
+	/**
+	 * @var EntityLoaderCollection
+	 */
 	private $_loaders;
 
+	/**
+	 * @var int | null
+	 */
 	private $_productID;
 
 	public function __construct(
@@ -23,20 +36,33 @@ class UnitProxy extends Unit
 		parent::__construct($locale, $priceTypes, $defaultCurrency);
 	}
 
+	/**
+	 * Set the product ID so a product can be lazy loaded
+	 *
+	 * @param $productID
+	 */
 	public function setProductID($productID)
 	{
-		if (!is_numeric($productID) and $productID != (int) $productID) {
+		if (!is_numeric($productID) && $productID != (int) $productID) {
 			throw new \InvalidArgumentException('Product ID must be a whole number');
 		}
 
 		$this->_productID = $productID;
 	}
 
+	/**
+	 * Get the product ID
+	 *
+	 * @return int|null
+	 */
 	public function getProductID()
 	{
 		return $this->_productID;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function setProduct($product)
 	{
 		if (null === $this->_productID) {
@@ -46,6 +72,9 @@ class UnitProxy extends Unit
 		return parent::setProduct($product);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function getProduct()
 	{
 		if ($this->_productID && !parent::getProduct() && $this->_loaders->exists('product')) {
@@ -56,6 +85,9 @@ class UnitProxy extends Unit
 		return parent::getProduct();
 	}
 
+	/**
+	 * Drop product on serialization
+	 */
 	public function __sleep()
 	{
 		return array_diff(array_keys(get_object_vars($this)), ['_product']);
