1879ee0b546f3057d00e3ce7a02e10eb37952d48
Samuel Trangmar-Keates <sam@message.co.uk>
2015-09-03 10:49:35 +0100

    Whitespace

--- src/Events.php
+++ src/Events.php
@@ -4,9 +4,9 @@ namespace Message\Mothership\Commerce;
 
 final class Events
 {
-	const PRODUCT_SELECTOR_BUILD     = 'commerce.product_selector.form.build';
-	const PRODUCT_SELECTOR_PROCESS   = 'commerce.product_selector.form.process';
-	const CURRENCY_CHANGE            = 'commerce.currency.change';
-	const SALES_REPORT               = 'commerce.report.sales-report';
-	const TRANSACTIONS_REPORT        = 'commerce.report.transaction-report';
+	const PRODUCT_SELECTOR_BUILD   = 'commerce.product_selector.form.build';
+	const PRODUCT_SELECTOR_PROCESS = 'commerce.product_selector.form.process';
+	const CURRENCY_CHANGE          = 'commerce.currency.change';
+	const SALES_REPORT             = 'commerce.report.sales-report';
+	const TRANSACTIONS_REPORT      = 'commerce.report.transaction-report';
 }
