459019e935bc5659169ee15b1297a060e5541965
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-10-15 15:52:45 +0100

    add docblocks to BarcodeEdit class

--- src/Product/Unit/BarcodeEdit.php
+++ src/Product/Unit/BarcodeEdit.php
@@ -5,6 +5,14 @@ namespace Message\Mothership\Commerce\Product\Unit;
 use Message\Cog\DB\Query;
 use Message\Mothership\Commerce\Product\Barcode\CodeGenerator\GeneratorInterface;
 
+/**
+ * Class BarcodeEdit
+ * @package Message\Mothership\Commerce\Product\Unit
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Class for saving barcodes against units using the registered barcode generator.
+ */
 class BarcodeEdit
 {
 	/**
@@ -17,12 +25,21 @@ class BarcodeEdit
 	 */
 	private $_generator;
 
+	/**
+	 * @param Query $query
+	 * @param GeneratorInterface $generator
+	 */
 	public function __construct(Query $query, GeneratorInterface $generator)
 	{
 		$this->_query = $query;
 		$this->_generator = $generator;
 	}
 
+	/**
+	 * Generates a barcode with the registered generator and save it to the database
+	 *
+	 * @param Unit $unit
+	 */
 	public function generateAndSave(Unit $unit)
 	{
 		$unit->barcode = $this->_generator->generateFromUnit($unit);
